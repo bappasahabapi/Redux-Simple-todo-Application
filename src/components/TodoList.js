1d8ddc12c6ec9fc3a54b17a6a11fecfac7551e5e
bappasahabapi <bappabappa840@gmail.com>
2022-08-21 22:33:41 +0600

    making the components

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList() {
+    return (
+        <div
+            className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto" >
+           
+           <Todo/>
+        </div>
+    )
+}

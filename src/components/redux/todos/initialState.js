60f5a7dc32ad21c2b8a064a6832efdf3f58516f4
bappasahabapi <bappabappa840@gmail.com>
2022-08-22 11:02:26 +0600

    Defining initialState for todo reducer

---
+++ src/components/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState =[
+    {
+        id:1 ,
+        text:'Hasib Vai Todo list Assignment',
+        completed:ture
+    },
+    {
+        id:2,
+        text:'Bappa da  Todo list Assignment',
+        completed:false,
+        color:'red'
+    }
+]
